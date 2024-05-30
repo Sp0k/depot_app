b3170c9c99c79db8726b2fcdd655a213017cbc61
Sp0k <contact@gabsavard.com>
2024-05-30 17:16:18 -0300

    feat(cart): d3 - adding a button
    - Added a button on the catalog page to add products to the cart
    - Display items in the cart on a cart page
    
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,9 +15,9 @@ class Product < ApplicationRecord
 
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
-    return if line_items.empty?
-
-    errors.add(:base, 'Line Items present')
-    throw :abort
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
   end
 end
